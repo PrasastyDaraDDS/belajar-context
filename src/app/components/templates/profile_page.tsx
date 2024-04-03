801967822946d4578a46c9ec3f187a6bf0c56d09
PrasastyDaraDSS <daraprasasty075@gmail.com>
2024-04-03 14:24:26 +0700

    W07: Jawaban soal 4

---
+++ src/app/components/templates/profile_page.tsx
@@ -0,0 +1,19 @@
+import Heading from "../atoms/heading";
+import Post from "../atoms/post";
+import Section2 from "../atoms/section2";
+import AllPosts from "../organisms/allpost";
+
+export default function ProfilePage() {
+    return (
+        <Section2 isFancy={true}>
+            <Heading>Profil Saya</Heading>
+            <Heading>Nama: Prasasty Dara DS</Heading>
+            <Heading>Nim: 2141720263</Heading>
+            <Post
+                title="Hello traveller!"
+                body="Baca tentang petualangan Saya."
+            />
+            <AllPosts />
+        </Section2>
+    );
+}
